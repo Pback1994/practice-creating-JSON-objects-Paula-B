f262cd9e1b6a87694909979b6547a285d5f94ecd
Paula Back <132527524+Pback1994@users.noreply.github.com>
2025-06-15 21:17:04 -0500

    Submitting Lesson 3 - Practice Creating JSON Objects.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # practice-creating-JSON-objects-Paula-B
+
+Goal: Create JSON object representing movie in catelog.
+
+- Title (string),
+- Year (number),
+- isAvailable (boolean),
+- Genre (array),
+- Cast (nested objects)
---
+++ practice.js
@@ -0,0 +1,18 @@
+let catalog = {
+  movie: "Gone With The Wind",
+  year: 1939,
+  isAvailable: false,
+  genre: [
+    "Romance",
+    "War",
+    "Comedy",
+    "Melodrama",
+    "Adventure",
+    "Drama",
+    "Historical Fiction",
+  ],
+  cast: [
+    { name: "Vivien Leigh", role: "Lead Female" },
+    { name: "Clark Gable", role: "Lead Male" },
+  ],
+};
